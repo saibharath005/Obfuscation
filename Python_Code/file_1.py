d882a0b8fa98fca2c9513379fbf0339425dafc1a
Sai Bharath Kothakota <sai.bharath@tredence.com>
2021-06-07 23:29:53 +0530

    Obfuscation Logic

---
+++ Python_Code/file_1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+ @Author
+ Sai Bharath Kothakota
+ saibharath005@gmail.com
+
+"""
+
+class Dummy_1(object):
+	def __init__(self,):
+		pass
+	def dummy_1(self):
+		prin("Python code inside file 1")
---
+++ Python_Code/file_2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+ @Author
+ Sai Bharath Kothakota
+ saibharath005@gmail.com
+
+"""
+
+class Dummy_2(object):
+	def __init__(self,):
+		pass
+	def dummy_2(self):
+		prin("Python code inside file 2")
